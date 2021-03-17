f443be76ff15d59f9b17fb756421fe78170093be
Billybatzzbeer16 <yannickrobijn10@gmail.com>
2021-03-17 17:27:48 +0100

    mongo

--- Parser.py
+++ Parser.py
@@ -7,7 +7,7 @@ import redis
 
 r = redis.Redis(host='redisdb', port=6379, db=0, charset="utf-8" ,decode_responses=True)
 
-myclient = mongo.MongoClient("mongodb://localhost:27017/")
+myclient = mongo.MongoClient("mongodb://mongodb:27017/")
 mydb = myclient["Bitcoin"]
 mycol = mydb["Values"]
 
